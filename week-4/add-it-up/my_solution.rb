cd5d06567bfbe5871577e516805c1fe0858554fc
DanielJPark <danpark80@gmail.com>
2016-03-03 19:41:50 -0500

    4.7 Peer Pairing

---
+++ week-4/add-it-up/my_solution.rb
@@ -0,0 +1,43 @@
+def total(array)
+	array.inject(0){|sum,x| sum + x }
+end
+
+total ([1,2,3])
+
+
+# Add it up!
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# I worked on this challenge [by myself, with: Elizabeth Alexander].
+
+# 0. total Pseudocode
+# make sure all pseudocode is commented out!
+
+# Input:
+# Output:
+# Steps to solve the problem.
+
+
+# 1. total initial solution
+
+
+
+# 3. total refactored solution
+
+
+
+# 4. sentence_maker pseudocode
+# make sure all pseudocode is commented out!
+# Input:
+# Output:
+# Steps to solve the problem.
+
+
+# 5. sentence_maker initial solution
+
+
+
+# 6. sentence_maker refactored solution
