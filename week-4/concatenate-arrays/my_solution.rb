486e1cab2e9811af15d1059097593c42b31d7698
DanielJPark <danpark80@gmail.com>
2016-03-06 11:14:35 -0500

    4.6.5 completed

---
+++ week-4/concatenate-arrays/my_solution.rb
@@ -0,0 +1,15 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def array_concat(array_1, array_2)
+  # Your code here
+  concated_array = array_1 + array_2
+  return concated_array
+end
+
+array_concat([1,2,3], [4,5,6])   # => [1,2,3,4,5,6]
+array_concat([10, 4], ["apple"]) # => [10, 4, "apple"]
