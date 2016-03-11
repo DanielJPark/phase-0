8140acab3152705712b33c52ef8ab96f87aef5d8
DanielJPark <danpark80@gmail.com>
2016-03-10 21:36:47 -0500

    getting annoyed

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -1,14 +1,9 @@
+
 # Calculate the mode Pairing Challenge
 
-<<<<<<< HEAD
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge [by myself, with: Elizabeth Alexander  ]
 
-# I spent [] hours on this challenge.
-=======
-# I worked on this challenge [by myself, with: Elizabeth ALexander]
-
-# I spent [4] hours on this challenge.
->>>>>>> 5.3
+# I spent 4 hours on this challenge.
 
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
@@ -18,73 +13,13 @@
 
 # 0. Pseudocode
 
-# What is the input?
-<<<<<<< HEAD
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
-=======
-	#Input is an Array.
-# What is the output? (i.e. What should the code return?)
-	#Output will be either the original Array or the value that occurs most frequently.
-# What are the steps needed to solve the problem?
-	#Step 1 - Find out if a value occurs more frequently than others within the array.
-	#Step 2 - Find a way to print those values
-	#Step 3 - If no value occurs more often than any other, print original array.
->>>>>>> 5.3
-
+# What is the input? The input is an array. 
+# What is the output? The output will either be the original array, or whichever value from the array occurs the most often. 
+# What are the steps needed to solve the problem? First, you need to figure out whether or not a value does occur more often then other numbers in the array. If so, you must find a way to print those values out. If not, you must simply print out the original array. 
 
 # 1. Initial Solution
 
-<<<<<<< HEAD
 
-
-
-# 3. Refactored Solution
-
-
-
-
-# 4. Reflection
-
-
-
-# Pad an Array
-
-# I worked on this challenge with: Alicia Briceland
-
-# I spent [] hours on this challenge.
-
-
-# Complete each step below according to the challenge directions and
-# include it in this file. Also make sure everything that isn't code
-# is commented in the file.
-
-
-
-
-def mode(arrays)
- freq = arrays.inject(Hash.new(0)) { |h,v| h[v] += 1; h } 
- max_value= arrays.max_by { |v| freq[v] }   
- 
-  
-end 
-
-mode([1,2,3,3,4,5])
-
-mode(["a","b","c","c","d"])
-
-mode([1,1,2,3,3,4])
-
-mode([1,2,3])
-
-
-# def mode(mode)
-#   mode_return = mode.inject({}) { |k, v| k[v] = mode.count(v); k }
-#   mode_return.select { |k,v| v == mode_return.values.max }.keys
-# end
-
-#puts mode([5, 6, 7, 7, 7, 8, 8, 8, 10, 10, 10, 15])
-=======
 #def mode(arrays)
 #freq = arrays.inject(Hash.new(0)) { |h,v| h[v] += 1; h } 
 #puts arrays.max_by { |v|v == highest_freq} 
@@ -97,17 +32,17 @@ mode([1,2,3])
 
 #mode([1,2,3,4,4,5,5])
 
-
-
 # 3. Refactored Solution
+
 def mode(arrays)
- freq = arrays.inject(Hash.new(0)) { |h,v| h[v] += 1; h } 
- highest_freq=freq.values.max
- mode_hash = freq.select {|key, value| value == highest_freq}
- mode_hash.keys
-end
+  freq = arrays.inject(Hash.new(0)) { |h,v| h[v] += 1; h } 
+  highest_freq=freq.values.max
+  mode_hash = freq.select {|key, value| value == highest_freq}
+  puts mode_hash.keys
+end 
 
 mode([1,2,3,4,4,5,5])
+
 
 
 
