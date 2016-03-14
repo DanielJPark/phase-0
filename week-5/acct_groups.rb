858d3892a097246ada6009379e373bd3b474974e
DanielJPark <danpark80@gmail.com>
2016-03-14 07:19:16 -0400

    5.6 work in progress

---
+++ week-5/acct_groups.rb
@@ -0,0 +1,18 @@
+#pseudocode
+=begin
+
+input: A list of names
+output: A list of those names in groups of 3, 4, or 5.
+steps:
+define a method that takes an array and divides it into groups of 3, 4, or 5.
+
+=end
+
+comic_characters = ["Superman", "Batman", "Wonder Woman", "Green Lantern", "Flash", "Spiderman", "Wolverine", "Captain America", "Ironman", "Black Widow", "Dare Devil", "Hulk", "Robin", "Professor X", "Ant-Man", "Supergirl", "Magneto" ]
+
+def acct_group(array)
+	
+end
+
+
+acct_group(comic_characters)
