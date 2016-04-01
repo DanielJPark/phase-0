1dd1809c6788421fcd12233dd76d03b6d1eab41c
DanielJPark <danpark80@gmail.com>
2016-04-01 00:14:45 -0500

    bingo update

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -27,16 +27,132 @@ class BingoBoard
 
   def initialize(board)
     @bingo_board = board
-    @letters = ["B","I","N","G","O"]
-    @numbers = rand(1..100)
+    @letters = ['B','I','N','G','O']
+    @numbers = rand(46..47)
   end
 
+  def welcome
+    puts "Welcome to an exciting game of Bingo!"
+  end
 
+  def draw
+    puts "The first number drawn is..."
+
+    @letter = @letters.sample
+
+    if @letter == @letters[0]
+      @number = @numbers
+
+    elsif @letter == @letters[1]
+      @number = @numbers
+  
+    elsif @letter == @letters[2]
+      @number = @numbers
+
+    elsif @letter == @letters[3]
+      @number = @numbers 
+
+    else @letter == @letters[4]
+      @number = @numbers
+    end
+  
+    print @letter
+    puts @number.to_s
+  end
+
+  def cross_off
+    if @letter == 'B'
+      for subarray in @bingo_board
+        if subarray[0].to_s == @number
+          subarray[0] = 'X'
+      
+        end
+      end
+  
+  elsif @letter == 'I'
+      for subarray in @bingo_board
+        if subarray[1].to_s == @number
+          subarray[1] = 'X'
+      
+        end
+      end
+
+  elsif @letter == 'N'
+      for subarray in @bingo_board
+        if subarray[2].to_s == @number
+          subarray[2] = 'X'
+      
+        end
+      end
+
+  elsif @letter == 'G'
+      for subarray in @bingo_board
+        if subarray[3].to_s == @number
+          subarray[3] = 'X'
+      
+        end
+      end
+  
+  elsif @letter == 'O'
+      for subarray in @bingo_board
+        if subarray[4].to_s == @number
+          subarray[4] = 'X'
+      
+        end
+      end 
+
+  end
 end
 
+def column
+  column_b = [@bingo_board[0][0], @bingo_board[1][0], @bingo_board[2][0], @bingo_board[3][0], @bingo_board[4][0]]
+  puts "Column B contains..."
+  puts column_b
+end
+
+def game_board
+
+    puts "B  I  N  G  O"
+    @bingo_board[0].each do |number|
+      print number
+      print " "
+    end
+    puts ""
+
+    @bingo_board[1].each do |number|
+      print number
+      print " "
+    end
+    puts ""
+    
+    @bingo_board[2].each do |number|
+      print number
+      print " "
+    end
+    puts ""
+    
+    @bingo_board[3].each do |number|
+      print number
+      print " "
+    end
+    puts ""
+    
+    @bingo_board[4].each do |number|
+      print number
+      print " "
+    end
+    puts ""
+  
+  end
+
+end
 # Refactored Solution
 
 
+
+  
+
+  
 
 #DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
 board = [[47, 44, 71, 8, 88],
@@ -46,6 +162,11 @@ board = [[47, 44, 71, 8, 88],
         [75, 70, 54, 80, 83]]
 
 new_game = BingoBoard.new(board)
+new_game.welcome
+new_game.column
+new_game.draw
+new_game.cross_off
+new_game.game_board
 
 
 #Reflection
